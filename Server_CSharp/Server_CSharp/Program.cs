f7ad898e06c61e707a4916a4d32a73b1420a4190
Pablo Gómez Calvo <pabgom03@ucm.es>
2018-11-14 15:18:14 +0100

    Primera version del servidor, se conecta y recibe mensajes del android.

---
+++ Server_CSharp/Server_CSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Server_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UDPSocket s = new UDPSocket();
+            s.Server("147.96.119.235", 54000);
+            Console.WriteLine("Servidor creado en puerto 54000 y con la ip 147.96.119.235");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
