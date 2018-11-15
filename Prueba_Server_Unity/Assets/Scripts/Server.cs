505194f236022d84b96d269ea586f3ef412b03e5
Pablo Gómez Calvo <pabgom03@ucm.es>
2018-11-15 19:24:38 +0100

    GOD commit

---
+++ Prueba_Server_Unity/Assets/Scripts/Server.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Server_CSharp;
+
+public class Server : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UDPSocket s = new UDPSocket();
+        Debug.Log("New hecho.");
+        s.Server("147.96.100.53", 54000);
+       Debug.Log("Servidor creado en puerto 54000 y con la ip 147.96.100.53");
+
+
+
+        Console.ReadLine();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
+
+namespace Server_CSharp
+{
+    public class UDPSocket
+    {
+        private Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        private const int bufSize = 8 * 1024;
+        private State state = new State();
+        private EndPoint epFrom = new IPEndPoint(IPAddress.Any, 0);
+        private AsyncCallback recv = null;
+
+        public class State
+        {
+            public byte[] buffer = new byte[bufSize];
+        }
+
+        public void Server(string address, int port)
+        {
+            _socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.ReuseAddress, true);
+            _socket.Bind(new IPEndPoint(IPAddress.Parse(address), port));
+            Debug.Log("Bind hecho,preparado para recibir mensajes.");
+            Receive();
+        }
+
+        public void Client(string address, int port)
+        {
+            _socket.Connect(IPAddress.Parse(address), port);
+            Receive();
+        }
+
+        public void Send(string text)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            _socket.BeginSend(data, 0, data.Length, SocketFlags.None, (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = _socket.EndSend(ar);
+                Debug.Log("SEND: "+bytes+", "+text);
+            }, state);
+        }
+
+        private void Receive()
+        {
+            _socket.BeginReceiveFrom(state.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv = (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = _socket.EndReceiveFrom(ar, ref epFrom);
+                _socket.BeginReceiveFrom(so.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv, so);
+                Debug.Log("RECV: " + epFrom.ToString()+" : " + bytes+ "," + Encoding.ASCII.GetString(so.buffer, 0, bytes));
+            }, state);
+        }
+    }
+}
