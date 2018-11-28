8d60de564327fc1e768ea4a7266875ce961bd3a5
Pablo Gómez Calvo <pabgom03@ucm.es>
2018-11-28 01:33:31 +0100

    Listo para entrega, control en android que mueve personaje en Unity

---
+++ Prueba_Server_Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Server server;
+    byte[] data;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, 0, 0);
+        data = server.MandoState();
+
+        if (data != null)
+        {
+            if (data[0] == 1)
+                transform.Translate(0, 1, 0);
+            if (data[1] == 1)
+                transform.Translate(0, -1, 0);
+            if (data[2] == 1)
+                transform.Translate(-1, 0, 0);
+            if (data[3] == 1)
+                transform.Translate(1, 0, 0);
+            if (data[4] == 1)
+                transform.Translate(0, 5, 0);
+            if (data[5] == 1)
+                transform.Translate(0, -5, 0);
+            if (data[6] == 1)
+                transform.Translate(-5, 0, 0);
+            if (data[7] == 1)
+                transform.Translate(5, 0, 0);
+        }
+    }
+}
